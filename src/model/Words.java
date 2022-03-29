13fee6e4d812923d295ccb679d76ef0162670f65
Santiago Gutiérrez <santicoguvi02@gmail.com>
2022-03-29 00:25:41 -0500

    First commit

---
+++ src/model/Words.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Words {
+	private String name;
+	private String animal;
+	private String country;
+	private String thing;
+	
+	public Words(String name, String animal, String country, String thing) {
+		this.name=name;
+		this.animal=animal;
+		this.country=country;
+		this.thing = thing;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAnimal() {
+		return animal;
+	}
+
+	public void setAnimal(String animal) {
+		this.animal = animal;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getThing() {
+		return thing;
+	}
+
+	public void setThing(String thing) {
+		this.thing = thing;
+	}
+}
